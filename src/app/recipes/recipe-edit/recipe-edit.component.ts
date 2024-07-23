74d490f5bb311cd408a015cf5ac69037c2461baf
Jordan Shayo <jordanhildebrand2012@gmail.com>
2024-07-23 15:49:48 +0100

    refactor the code, apply more routing to different components

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrl: './recipe-edit.component.css',
+})
+export class RecipeEditComponent implements OnInit {
+  editMode = false;
+  id: number;
+
+  private route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+    });
+  }
+}
